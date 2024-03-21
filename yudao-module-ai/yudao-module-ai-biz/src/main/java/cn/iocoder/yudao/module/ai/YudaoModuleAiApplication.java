3f6a2201d0e8b8256642d9da1bee5640dd5f2a63
throwAndCatch <programmer.zhoup@gmail.com>
2024-03-21 11:32:47 +0800

    add ai module

---
+++ yudao-module-ai/yudao-module-ai-biz/src/main/java/cn/iocoder/yudao/module/ai/YudaoModuleAiApplication.java
@@ -0,0 +1,13 @@
+package cn.iocoder.yudao.module.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YudaoModuleAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YudaoModuleAiApplication.class, args);
+    }
+
+}
---
+++ yudao-module-ai/yudao-module-ai-biz/src/test/java/cn/iocoder/yudao/yudao/module/ai/YudaoModuleAiBizApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.iocoder.yudao.yudao.module.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YudaoModuleAiBizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
